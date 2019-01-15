69237ca698051fca40e37e7e05c3ba3addfbd7c5
burzychs <burzychs@gmail.com>
2019-01-15 20:57:32 +0100

    add permissions

--- api/settings.py
+++ api/settings.py
@@ -138,6 +138,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
 }
 
 # JWT settings
