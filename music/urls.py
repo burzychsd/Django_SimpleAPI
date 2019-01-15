ca025b5923487dea7169619134e73171630e6659
burzychs <burzychs@gmail.com>
2019-01-15 18:20:21 +0100

    simple crud implementation

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListSongsView
+from .views import ListSongsView, SongDetailView
 
 urlpatterns = [
-    path('songs/', ListSongsView.as_view(), name="songs-all")
+    path('songs/', ListSongsView.as_view(), name="songs-all"),
+    path('songs/<int:id>', SongDetailView.as_view(), name="song-detail")
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import status
 from .models import Song
 from .serializer import SongsSerializer
 
@@ -6,3 +8,56 @@ from .serializer import SongsSerializer
 class ListSongsView(generics.ListAPIView):
     queryset = Song.objects.all()
     serializer_class = SongsSerializer
+
+    def post(self, request, *args, **kwargs):
+        a_song = Song.objects.create(
+            title=request.data['title'],
+            artist=request.data['artist']
+        )
+        return Response(
+            data=SongsSerializer(a_song).data,
+            status=status.HTTP_201_CREATED
+        )
+
+class SongDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongsSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+            a_song = self.queryset.get(id=kwargs['id'])
+            return Response(SongsSerializer(a_song).data)
+        except Song.DoesNotExist:
+            return Response(
+                data={
+                    'message': 'Song with id: {} does not exist'.format(kwargs['id'])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+    
+    def put(self, request, *args, **kwargs):
+        try:
+            a_song = self.queryset.get(id=kwargs['id'])
+            serializer = SongsSerializer()
+            updated_song = serializer.update(a_song, request.data)
+            return Response(SongsSerializer(updated_song).data)
+        except Song.DoesNotExist:
+            return Response(
+                data={
+                    'message': 'Song with id: {} does not exist'.format(kwargs['id'])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            a_song = self.queryset.get(id=kwargs['id'])
+            a_song.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Song.DoesNotExist:
+             return Response(
+                data={
+                    'message': 'Song with id: {} does not exist'.format(kwargs['id'])
+                },
+                status=status.HTTP_404_NOT_FOUND
+            )
